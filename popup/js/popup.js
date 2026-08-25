b77794297e8a798b3be4351257c2cbe208d6c06d
emyX122 <josquin@lecharron.ch>
2026-08-25 20:36:40 +0200

    Remove support button and add report an issue button

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -3,3 +3,7 @@ console.log('PybrickCodeBlock is runing !!');
 document.getElementById("supportButton").addEventListener("click", () => {
     window.open('https://github.com/emyX122', '_blank');
 });
+
+document.getElementById("imageGithub").addEventListener("click", () => {
+    window.open('https://github.com/emyX122/PyBrick-open-code-block');
+});
