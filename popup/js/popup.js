a0d1513d0d88b94ff09f5fff33798c9bfff062ef
emyX122 <josquin@lecharron.ch>
2026-08-25 22:26:24 +0200

    update github links on the popup menu and add the avtar image profil

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,9 +1,13 @@
 console.log('PybrickCodeBlock is runing !!');
 
-document.getElementById("supportButton").addEventListener("click", () => {
-    window.open('https://github.com/emyX122', '_blank');
+document.getElementById("issueButton").addEventListener("click", () => {
+    window.open('https://github.com/emyX122/PyBrick-open-code-block/issues', '_blank');
 });
 
 document.getElementById("imageGithub").addEventListener("click", () => {
-    window.open('https://github.com/emyX122/PyBrick-open-code-block');
+    window.open('https://github.com/emyX122/PyBrick-open-code-block', '_blank');
+});
+
+document.getElementById("avatarImage").addEventListener("click", () => {
+    window.open('https://github.com/emyX122', '_blank');
 });
