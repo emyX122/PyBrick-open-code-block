0e4f975538143e4ae3d65195a8baeb4657c2c7d4
emyX122 <josquin@lecharron.ch>
2026-08-28 23:49:15 +0200

    add a message fore beta information and pass on public respository

--- initialSetup/js/initialisationVariables.js
+++ initialSetup/js/initialisationVariables.js
@@ -5,3 +5,6 @@
 let allKeys = "";
 let matchingKeys = "";
 let controleActivCode = null;
+
+//message de développement --------------------------------------------------------------------------------------
+alert("!Warning! Please note that this extension is still in beta and under active development. You can try it out to get an idea of what it will look like and where it stands, but it is not yet ready for real-world use.");
